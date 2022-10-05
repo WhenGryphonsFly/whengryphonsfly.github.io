069ffac754d496384b5b243951e13894df85a9d4
WhenGryphonsFly <84215159+WhenGryphonsFly@users.noreply.github.com>
2022-10-05 12:05:26 -0500

    Update dynamicBoxTest1.js

--- test/rgTests/dynamicBoxTest1.js
+++ test/rgTests/dynamicBoxTest1.js
@@ -1,5 +1,7 @@
 window.addEventListener("DOMContentLoaded", async ()=>{
+	console.log("Event listener fired");
 	const response = await fetch("https://whengryphonsfly.github.io/test/rgTests/dynamicBoxTest1.json");
+	console.log("Event listener obtained JSON");
 	
 	if (!response.ok) {
 		alert("An error has occured. Please refresh the page."); // TODO Put this on the document itself
@@ -13,6 +15,7 @@ window.addEventListener("DOMContentLoaded", async ()=>{
 		return;
 	}
 	
+	console.log("JSON obtained successfully");
 	for (threeDEvent in data.body.events) {
 		makeTableEntry(threeDEvent);
 	}
@@ -20,6 +23,7 @@ window.addEventListener("DOMContentLoaded", async ()=>{
 });
 
 function makeTableEntry(data): void {
+	console.log("makeTableEntry called");
 	const tableEntryList = document.getElementById("boxList");
 	const tableEntry = document.createElement("li");
 	const tableEntryHeader = document.createElement("div");
