d29554b94faccca606a84cc4877364b87d4fa131
WhenGryphonsFly <84215159+WhenGryphonsFly@users.noreply.github.com>
2022-10-05 12:01:27 -0500

    Add dynamic box test

---
+++ test/rgTests/dynamicBoxTest1.js
@@ -0,0 +1,33 @@
+window.addEventListener("DOMContentLoaded", async ()=>{
+	const response = await fetch("https://whengryphonsfly.github.io/test/rgTests/dynamicBoxTest1.json");
+	
+	if (!response.ok) {
+		alert("An error has occured. Please refresh the page."); // TODO Put this on the document itself
+		return;
+	}
+	
+	const data = await response.json();
+	
+	if (!data.body.events) {
+		alert("There are currently no events defined. Please define a new event.");
+		return;
+	}
+	
+	for (threeDEvent in data.body.events) {
+		makeTableEntry(threeDEvent);
+	}
+	
+});
+
+function makeTableEntry(data): void {
+	const tableEntryList = document.getElementById("boxList");
+	const tableEntry = document.createElement("li");
+	const tableEntryHeader = document.createElement("div");
+	
+	tableEntryHeader.setAttribute("class", "boxHeading");
+	tableEntryHeader.innerHTML += "Test Heading"; // TODO possible security vulnerability
+	tableEntry.appendChild(tableEntryHeader);
+	
+	tableEntry.innerHTML += "Test Body"; // TODO possible security vulnerability
+	tableEntryList.appendChild(tableEntry);
+}
