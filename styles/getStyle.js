c01046c09a0b97d431e48a2c8717be35ab3a8826
WhenGryphonsFly <84215159+WhenGryphonsFly@users.noreply.github.com>
2022-10-21 21:28:11 -0500

    Update getStyle.js

--- styles/getStyle.js
+++ styles/getStyle.js
@@ -1,7 +1,7 @@
 (function() {
     let queryString = (new URL(document.location)).searchParams;
     let head = document.getElementsByTagName("head")[0];
-    if (queryString.get("dark").toLowerCase() === "true") {
+    if (queryString.get("dark") != null && queryString.get("dark").toLowerCase() === "true") {
         head.innerHTML += "<link rel=\"stylesheet\" href=\"https://whengryphonsfly.github.io/styles/dark.css\">";
     } else {
         head.innerHTML += "<link rel=\"stylesheet\" href=\"https://whengryphonsfly.github.io/styles/light.css\">";
