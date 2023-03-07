9e0c5cc8733418ec712eba7d41b835e5f049c679
WhenGryphonsFly <84215159+WhenGryphonsFly@users.noreply.github.com>
2023-03-07 17:44:45 -0600

    Update encryptor.sh

--- encryptor.sh
+++ encryptor.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-# $1 is direcotry, #2 is subdirectories and filename, $3 is password
-staticrypt "./$1_decrypted/$2" $3 --config false --noremember --output ./$1/$2 --title "Password Protected"
+# $1 is directory, #2 is subdirectories and filename, $3 is password
+read -s password
+staticrypt "./$1_decrypted/$2" $password --config false --noremember --output ./$1/$2 --title "Password Protected"
