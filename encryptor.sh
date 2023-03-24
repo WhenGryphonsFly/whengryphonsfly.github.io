a7a855b359bbbce69dc5335d20ecf484cc8371ae
WhenGryphonsFly <84215159+WhenGryphonsFly@users.noreply.github.com>
2023-03-24 13:45:06 -0500

    Update encryptor.sh

--- encryptor.sh
+++ encryptor.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-# $1 is directory, #2 is subdirectories and filename, $3 is password
+# $1 is directory, #2 is subdirectories and filename
+echo "Enter password:"
 read -s password
-staticrypt "./$1_decrypted/$2" $password --config false --noremember --output ./$1/$2 --title "Password Protected"
+echo "Confirm password:"
+read -s passwordCheck
+
+if [[ $password == $passwordCheck ]]; 
+then staticrypt "./$1_decrypted/$2" $password --config false --noremember --output ./$1/$2 --title "Password Protected"; echo "Success!";
+else echo "Passwords do not match!";
+fi
